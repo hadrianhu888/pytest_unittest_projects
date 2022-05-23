e3347002587921bedfff9bdf272c72fc4a8f0b4d
hh888 <hadrian.hu@gmail.com>
2022-05-23 14:29:22 -0400

    added pytest file

---
+++ car/car_class.py
@@ -0,0 +1,27 @@
+#car.py class 
+
+import sys 
+import pytest 
+
+sys.path.append(".")
+
+class car():   
+    def __init__(self,name):        
+        self.name = name 
+        return self        
+    def factory(self,make):
+        self.make = make
+        return make 
+    def model_year(self,year): 
+        self.year = year
+        return year
+    def turnOn(self,status):
+        self.status = status
+        return status
+    def speed(self,velocity): 
+        self.velocity = velocity
+        return velocity
+    def powermode(self,vehicle_power_mode):
+        self.vehicle_power_mode = vehicle_power_mode
+        return vehicle_power_mode
+    
