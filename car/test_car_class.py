2d9b364014c858233a595b43517b8da35b89d457
hadrianhu888 <hadrian.hu@gmail.com>
2022-05-23 14:39:14 -0400

    Add files via upload

---
+++ car/test_car_class.py
@@ -0,0 +1,13 @@
+import car_class
+import pytest as pt 
+import sys
+sys.path.append(".")
+from car_class import car 
+car1 = car("Ford")
+def test_car_initialization(car1): 
+    assert car1.__init__("Ford") == "Ford"  
+
+
+
+
+
