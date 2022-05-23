abd6afa92a480ef548cc257e1cd58e27b3b278ca
hh888 <hadrian.hu@gmail.com>
2022-05-23 14:30:51 -0400

    test assert

--- car/test_car_class.py
+++ car/test_car_class.py
@@ -5,8 +5,7 @@ sys.path.append(".")
 from car_class import car 
 car1 = car("Ford")
 def test_car_initialization(car1): 
-    assert car1.__init__
-    
+    assert car1.__init__("Ford") == "Ford"  
 
 
 
